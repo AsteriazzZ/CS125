d35d784f99ad042a1517cb6c211ad2c4d43dc4c1
zzhan145 <zzhan145@391430f4-0411-4dd5-8b27-250df5562fc7>
2016-01-20 07:33:30 +0000

    changed

--- TakeOverTheWorld/printlns.java
+++ TakeOverTheWorld/printlns.java
@@ -3,7 +3,10 @@ public class printlns {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		//this is my file.
+		
+		for (int i = 0; i > 10; i++)
+			System.out.println(*);
+	}	
 
 }

22d00368784bd5fcda7aa369d81d578796e06db9
zzhan145 <zzhan145@391430f4-0411-4dd5-8b27-250df5562fc7>
2016-02-10 04:54:47 +0000

    2nd part: MyScriptPrinter is done!

--- Challenge2-Hollywood/MyScriptPrinter.java
+++ Challenge2-Hollywood/MyScriptPrinter.java
@@ -3,7 +3,7 @@
  * A program to print one actor's lines. 
  * See ScriptPrinter.txt for more information.
  * TODO: add your netid to the line below
- * @author put-your-netid-here
+ * @author zzhan145
  */
 public class MyScriptPrinter {
 	/**
@@ -16,30 +16,44 @@ public class MyScriptPrinter {
 		
 		TextIO.putln("Which character's lines would you like? (NEO,MORPHEUS,ORACLE)");
 		name = TextIO.getln();
+		name = name.toUpperCase();
 
 		TextIO.readFile("thematrix.txt"); // stop reading from the keyboard- use the script
 
 		//TODO: Print the name here (see ScriptPrinter.txt example output for format)
 		
 		output = false; // initially don't print anything
+		TextIO.putln(name + "'s lines:");
 
 		// This loop will read one line at a time from the script until it
 		// reaches the end of the file and then TextIO.eof() will return true.
 		// eof means end-of-file
+		
 		while (false == TextIO.eof()) {
 			String line = TextIO.getln(); // Read the next line
-			//TODO: If it's a blank line set 'output' to false			
-			//TODO: Correct the output format (see ScriptPrinter.txt example output)
+			
 			//TODO: Re-order the three if statements so the output is correct
 
-			if (line.indexOf(name) >= 0)
+			if (line.indexOf(name) >= 0){
 				output = true; // We found the character's name, time to start printing their lines
+			}else{
+				output = false;
+			}
 
-			if (output)
-				TextIO.putln(line); // Only print the line if 'output' is true
-
+			if (output){
+				line = TextIO.getln();
+				//TODO: Correct the output format (see ScriptPrinter.txt example output)
+				line = line.trim();
+				TextIO.putln(name + ":\"" + line + "\""); // Only print the line if 'output' is true
+			}
+			
+			//TODO: If it's a blank line set 'output' to false
+			if(line == ""){
+				output = false;
+			}
 		}
 		//TODO: Print 3 dashes here to indicate processing is complete
+		TextIO.putln("---");
 	}
 
 }

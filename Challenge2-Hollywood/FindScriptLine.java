9d8442bc1747acb0e67f6a095a5faa1d26037e46
zzhan145 <zzhan145@391430f4-0411-4dd5-8b27-250df5562fc7>
2016-02-10 05:49:29 +0000

    MP2 is DONE!!! Yoo~hoo~hoo~

--- Challenge2-Hollywood/FindScriptLine.java
+++ Challenge2-Hollywood/FindScriptLine.java
@@ -3,12 +3,51 @@
  * A program to search for specific lines and print their line number.
  * See FindScriptLine.txt for more details
  * TODO: add your netid to the line below
- * @author put-your-netid-here
+ * @author zzhan145
  */
 public class FindScriptLine {
 
 	public static void main(String[] args) {
 // TODO: Implement the functionality described in FindScriptLine.txt
-// TODO: Test your code manually and using the automated unit tests in FindScriptLineTest		
+// TODO: Test your code manually and using the automated unit tests in FindScriptLineTest
+		
+		//When the program starts it asks you to enter the words to search for.
+		TextIO.putln("Please enter the word(s) to search for");
+		
+		String word;
+		word = TextIO.getln();
+		int count = 0;
+		boolean output = false;
+		
+		//read from the file
+		TextIO.readFile("thematrix.txt");
+		TextIO.putln("Searching for " + "\'" + word + "\'");
+		
+		//search until we reach the end of the file
+		while(false == TextIO.eof()){
+			
+			//the program reads one line at a time
+			String line = TextIO.getln();
+			//the test is case insensitive
+			String lineUpperCase = line.toUpperCase();
+			String wordUpperCase = word.toUpperCase();
+			
+			//searching for the word you gave in the line
+			if(lineUpperCase.indexOf(wordUpperCase) >= 0){
+				output = true;
+			}else{
+				output = false;	
+			}
+			
+			//counting for the numbers
+			count = count + 1;
+			
+			//when we find the word, output the line number with the script, connected by a dash
+			if(output){
+				line = line.trim();
+				TextIO.putln(count + " - " + line);
+			}
+		}
+		TextIO.putln("Done Searching for " + "\'" + word + "\'");
 	}
 }

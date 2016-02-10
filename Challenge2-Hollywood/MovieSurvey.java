32c1d439deebd28b8cbc36a4f0c409679cd1d1eb
zzhan145 <zzhan145@391430f4-0411-4dd5-8b27-250df5562fc7>
2016-02-10 03:52:35 +0000

    1st part: Movie Survey done!

--- Challenge2-Hollywood/MovieSurvey.java
+++ Challenge2-Hollywood/MovieSurvey.java
@@ -3,17 +3,52 @@
  * A program to run a simple survey and report the results. See MovieSurvey.txt
  * for more information. TODO: add your netid to the line below
  * 
- * @author put-your-netid-here
+ * @author zzhan145
  */
 public class MovieSurvey {
 	public static void main(String[] arg) {
-		// TODO: Write your program here
-		// Hints :
-		// Formatted output
-		// http://math.hws.edu/javanotes/c2/s4.html#basics.4.4
+		int cinema;
+		int dvdPlayer;
+		int computer;
 		
-		// Don't just copy paste the expected output
-		// For grading purposes your code may be tested
-		// with other input values.
+		//some introductory lines
+		TextIO.putln("Welcome. We're interested in how people are watching movies this year.");
+		TextIO.putln("Thanks for your time. - The WRITERS GUILD OF AMERICA.");
+		TextIO.putln("Please tell us about how you've watched movies in the last month.");
+		
+		//prompt for three quantities
+		//The first one
+		TextIO.putln("How many movies have you seen at the cinema?");
+		cinema = TextIO.getlnInt();
+		
+		//The second one
+		TextIO.putln("How many movies have you seen using a DVD or VHS player?");
+		dvdPlayer = TextIO.getlnInt();
+		
+		//The third one
+		TextIO.putln("How many movies have you seen using a Computer?");
+		computer = TextIO.getlnInt();
+		
+		//print out a summary of the input values
+		TextIO.put("Summary: ");
+		TextIO.put(cinema + " Cinema movies, ");
+		TextIO.put(dvdPlayer + " DVD/VHS movies, ");
+		TextIO.putln(computer + " Computer movies");
+		
+		//print out the total number of movies seen in one month
+		int total = cinema + dvdPlayer + computer;
+		TextIO.putln("Total: " + total + " movies");
+		
+		//print out the percent of movies seen at the cinema to two decimal places
+		double percentOfCinema;
+		percentOfCinema = ((float)cinema / (float)total) * 100;
+		TextIO.putf("Fraction of movies seen at a cinema: %.2f%%%n", percentOfCinema);
+		
+		//print out the percent of movies seen outside of the cinema to two decimal places
+		double percentOfOutside;
+		percentOfOutside = 100 - percentOfCinema;
+		TextIO.putf("Fraction of movies seen outside of a cinema: %.2f%%%n", percentOfOutside);
+		
+		
 	}
 }

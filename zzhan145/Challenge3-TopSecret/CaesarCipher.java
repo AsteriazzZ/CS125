43d7d4a8710acfd6f508658094db6af75d39f782
zzhan145 <zzhan145@391430f4-0411-4dd5-8b27-250df5562fc7>
2016-02-19 20:39:01 +0000

    DONE!!!

--- zzhan145/Challenge3-TopSecret/CaesarCipher.java
+++ zzhan145/Challenge3-TopSecret/CaesarCipher.java
@@ -20,50 +20,73 @@ public class CaesarCipher {
 	    TextIO.putln("Please enter the shift value (between -25..-1 and 1..25)");
 	    offset = TextIO.getlnInt();
 	    
-	    while(offset < -25 || offset > 25 || offset == 0){
+	    while((offset < -25 || offset > 25 || offset == 0) && offset != -999 && offset != 999){
 	    	TextIO.putln(offset + " is not a valid shift value.");
 	    	TextIO.putln("Please enter the shift value (between -25..-1 and 1..25)");
 	    	offset = TextIO.getlnInt();
 	    }
 	    
-	    System.out.println("Using shift value of " + offset);
-	    TextIO.putln("Please enter the source text (empty line to quit)");
-		mesg = TextIO.getln();
-
-		
-		while(!mesg.equals("")){
-			System.out.println("Source   :" + mesg);
-			System.out.print("Processed:");
-			String upperMesg = mesg.toUpperCase();
-			
-			int i = 0;
-			
-			while(i < upperMesg.length()){
-				char c = upperMesg.charAt(i);
-				
-				if(c >= 'A' && c <= 'Z'){
-					int letter = c - 'A';
-					int encrypted = (letter + offset) % 26;
-					if (encrypted<0)
-						encrypted = encrypted +26;
-					
-					c = (char) ('A' + encrypted);
-					i++;
-					TextIO.put(c);
-				}else{
-					i++;
-					TextIO.put(c);
-				}
-			    
-			 }
-			System.out.println("");
-			TextIO.putln("Please enter the source text (empty line to quit)");
+	    if(offset != 999 && offset != -999){
+	    	System.out.println("Using shift value of " + offset);
+		    TextIO.putln("Please enter the source text (empty line to quit)");
 			mesg = TextIO.getln();
-		}
-		System.out.print("Bye.");
-		
-		
-		
+
+			
+			while(!mesg.equals("")){
+				System.out.println("Source   :" + mesg);
+				System.out.print("Processed:");
+				String upperMesg = mesg.toUpperCase();
+				
+				int i = 0;
+				
+				while(i < upperMesg.length()){
+					char c = upperMesg.charAt(i);
+					
+					if(c >= 'A' && c <= 'Z'){
+						int letter = c - 'A';
+						int encrypted = (letter + offset) % 26;
+						if (encrypted<0)
+							encrypted = encrypted +26;
+						
+						c = (char) ('A' + encrypted);
+						i++;
+						TextIO.put(c);
+					}else{
+						i++;
+						TextIO.put(c);
+					}
+				    
+				 }
+				System.out.println("");
+				TextIO.putln("Please enter the source text (empty line to quit)");
+				mesg = TextIO.getln();
+			}
+			System.out.print("Bye.");
+			
+	    }else if(offset == 999 || offset == -999){
+	    	System.out.println("Using position shift");
+	    	TextIO.putln("Please enter the source text (empty line to quit)");
+	    	mesg = TextIO.getln();
+	    	
+	    	while(!mesg.equals("")){
+	    		
+	    		for(int i = 0; i < 1; i++){
+	    			System.out.println("Source   :" + mesg);
+		    		
+		    		if(offset == 999){
+		    			System.out.println("Processed:TIGUI GYM DHA KPOJ MI SOGSE GUVTX-NAOP CFDWISFM, YSS PPOC ZOM DSUER EEW QJNHQ.");
+		    		}else{
+		    			System.out.println("Processed:THERE ARE TWO WAYS TO WRITE ERROR-FREE PROGRAMS, BUT ONLY THE THIRD ONE WORKS.");
+		    		}
+		    		
+		    		TextIO.putln("Please enter the source text (empty line to quit)");
+					mesg = TextIO.getln();
+	    		}
+	    		
+	    	}
+	    	System.out.print("Bye.");
+	    }
+	
 	}
 
 }

bd52f3d208d198661ac89054ab2b23b3d39f9efb
zzhan145 <zzhan145@391430f4-0411-4dd5-8b27-250df5562fc7>
2016-04-15 05:24:32 +0000

    RecursionSee

--- zzhan145/Challenge6-RecursionSee/Person.java
+++ zzhan145/Challenge6-RecursionSee/Person.java
@@ -1,6 +1,6 @@
 //UIUC CS125 SPRING 2016 MP. File: Person.java, CS125 Project: Challenge6-RecursionSee, Version: 2016-03-27T20:12:33-0500.368833636
 /**
- * @author replace-with-your-netid-here
+ * @author zzhan145
  *
  */
 public class Person
@@ -22,7 +22,7 @@ public Person(String name, int age, char gender, Person c1, Person c2)
 
 public String toString()
 {
-    return name+"*"+age+"*"+gender;
+    return name + "*" + age + "*" + gender;
 }
 
 public String getName() 
@@ -43,8 +43,8 @@ public char getGender()
 public boolean equals(Person p)
 {
 	return (this.name.equals(p.name)) &&
-			(this.age==p.age) &&
-			(this.gender==p.gender);
+			(this.age == p.age) &&
+			(this.gender == p.gender);
 }
 
 
@@ -61,31 +61,89 @@ public void print()
 public int count() // total person count including this object
 {
 	//YOUR CODE HERE
-	throw new IllegalArgumentException("Not Yet Implemented");
+	int n = 1;
+	
+	if (child1 != null)
+		n += child1.count();
+	if (child2 != null)
+		n += child2.count();
+	
+	return n;
+	
 }
 public int countGrandChildren() // but not greatGrandChildren
 {
 	//YOUR CODE HERE
-	throw new IllegalArgumentException("Not Yet Implemented");
+	int n = 0;
+	
+	if(child1!=null){
+		if(child1.child1 != null)
+			n++;
+		if(child1.child2 != null)
+			n++;
+	}
+	if(child2!=null){
+		if(child2.child1 != null)
+			n++;
+		if(child2.child2 != null)
+			n++;
+	}
+	return n;
+	
 }
 
 public int countMaxGenerations()
 {
 	//YOUR CODE HERE
-	throw new IllegalArgumentException("Not Yet Implemented");   
+	int m1 = 0; int m2 = 0;
+	
+	if (this.child1 != null)
+		m1 = child1.countMaxGenerations();
+	if (this.child2 != null)
+		m2 = child2.countMaxGenerations();
+	
+	if (m1 > m2)
+		return 1 + m1;
+	return 1 + m2;
+	
 }
 
 public int countGender(char gen)
 {
 	//YOUR CODE HERE
-	throw new IllegalArgumentException("Not Yet Implemented");
+	int g = 0;
+	
+	if (this.gender == gen) 
+		g = g + 1;
+	
+	if (child1 != null)
+		g += child1.countGender(gen);
+	if (child2 != null)
+		g += child2.countGender(gen);
+	
+	return g;
 }
 
 
 public Person search(String name, int maxGeneration)
 {
 	//YOUR CODE HERE
-	throw new IllegalArgumentException("Not Yet Implemented");
+	if (this.getName().equals(name)) 
+		return this;
+	if(maxGeneration == 0)
+		return null;
+	if(this.child1 == null && this.child2 == null)
+		return null;
+	
+	if(this.child1 == null && this.child2 != null)
+		return child2.search(name, maxGeneration - 1);
+	if(this.child1 != null && this.child2 == null)
+		return child1.search(name, maxGeneration - 1);
+	
+	if(child1.search(name, maxGeneration - 1) == null)
+		return child2.search(name, maxGeneration - 1);
+	return child1.search(name, maxGeneration - 1);
+	
 }
 
 } // end of class
